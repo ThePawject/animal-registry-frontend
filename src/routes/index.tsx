f7264a009ece82ad698043ca948ba76f6fac84ac
Dominik Zając <dominik.zajac@kruko.io>
2026-02-10 22:48:06 +0100

    fix: missing prop in useUserInfo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,6 +22,7 @@ function App() {
 
   const { shelterName, isLoadingRoles } = useUserInfo({
     setIsLoginModalOpen,
+    isLoginModalOpen,
     setRoles,
   })
   useAxiosWithAuth({ setIsLoginModalOpen })
